6e4fa0ae7278bbb0f49af4ff6be882736d78dce6
Ludwig Stecher <ludwig.stecher@gmx.de>
2025-10-05 18:22:29 +0200

    increase post count

--- astro.config.ts
+++ astro.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
           title: "Aloso's Blog",
           metrics: { readingTime: true },
           navigation: 'none',
+          postCount: 15,
         }),
       ],
       head: [
